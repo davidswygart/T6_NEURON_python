4ccc6a7b46cbee66ec7e26076fe11322ff86bddd
davidswygart <davidswygart2015@u.northwestern.edu>
2020-02-03 09:27:48 -0600

    Changed model to class
    I hcnaged the whole modell to a class and can now create the model as an object.

--- T6_Sim.py
+++ T6_Sim.py
@@ -8,23 +8,24 @@ from Electrodes import placeCurrentClamp
 from Electrodes import placeVoltageClamp
 
 
-def main():
-    #cd C:/Users/david/Box/T6_BP_NEURON_SIM/T6_NEURON_python
-    settings = Settings()                                                       #Load settings (eg. experimental setup, physiology parameters, display settings)    
-    axons = f.loadMorph(h, "T6_V3.hoc")                                         #Load axon morphology
-    ribRec = f.recordRibbons(h, "InputData/RibbonLocations.txt")                #A list of voltage recording vectors at each ribbon
-    segRec = f.recordSegments(h)                                                #A list of voltage recording vectors at each segment
-    inhSyns = InhSyns(h, "InputData/InhSynLocations.txt", settings)             #An object containing all inhibitory synapses, their presynaptic stimulation, and their connector
-    iClamp_baselineExc = placeCurrentClamp(h, axons[0], 0, 0, settings.tstop, settings.BaselineExc) #section, location on the section, delay, duration, amplitude
-    iClamp_visExc = placeCurrentClamp(h, axons[0], 0, settings.ExcStart, settings.ExcEnd - settings.ExcStart, settings.ExcAmp) #section, location on the section, delay, duration, amplitude
-    
-    if settings.DoVClamp:
-        settings.v_init = settings.Hold1
-        volClamp = placeVoltageClamp(h, axons[0], 0, settings)
-        iRecord = h.Vector().record(volClamp._ref_i)
-        f.runSim(h, settings, inhSyns, segRec, ribRec)
-        f.makePlot(np.linspace(0, settings.tstop, len(segRec[0])), iRecord)
-    else:
-        f.runSim(h, settings, inhSyns, segRec, ribRec)
+class model():  
+    def __init__(self):
+        #cd C:/Users/david/Box/T6_BP_NEURON_SIM/T6_NEURON_python
+        self.settings = Settings()                                                       #Load settings (eg. experimental setup, physiology parameters, display settings)    
+        self.axons = f.loadMorph(h, "T6_V3.hoc")                                         #Load axon morphology
+        self.ribRec = f.recordRibbons(h, "InputData/RibbonLocations.txt")                #A list of voltage recording vectors at each ribbon
+        self.segRec = f.recordSegments(h)                                                #A list of voltage recording vectors at each segment
+        self.inhSyns = InhSyns(h, "InputData/InhSynLocations.txt", self.settings)             #An object containing all inhibitory synapses, their presynaptic stimulation, and their connector
+        self.iClamp_baselineExc = placeCurrentClamp(h, self.axons[0], 0, 0, self.settings.tstop, self.settings.BaselineExc) #section, location on the section, delay, duration, amplitude
+        self.iClamp_visExc = placeCurrentClamp(h, self.axons[0], 0, self.settings.ExcStart, self.settings.ExcEnd - self.settings.ExcStart, self.settings.ExcAmp) #section, location on the section, delay, duration, amplitude
+        
+        if self.settings.DoVClamp:
+            self.settings.v_init = self.settings.Hold1
+            self.volClamp = placeVoltageClamp(h, self.axons[0], 0, self.settings)
+            self.iRecord = h.Vector().record(self.volClamp._ref_i)
+            f.runSim(h, self.settings, self.inhSyns, self.segRec, self.ribRec)
+            f.makePlot(np.linspace(0, self.settings.tstop, len(self.segRec[0])), self.iRecord)
+        else:
+            f.runSim(h, self.settings, self.inhSyns, self.segRec, self.ribRec)
 
-main()
+T6 = model()
