38c17d1b646caa872b3d97329a9bb1c7a7ecba2a
davidswygart <davidswygart2015@u.northwestern.edu>
2020-05-15 15:50:37 -0500

    getting rid of results

--- settings.py
+++ settings.py
@@ -23,39 +23,32 @@ class Settings ():
         #Voltage Clamp Mode (optional)
         self.DoVClamp = 1                                               # Set to 1 to run in voltage clamp configuration
         
-        self.Hold1 = 0                                      # Initial holding potential (mV)      
+        self.Hold1 = -60                                      # Initial holding potential (mV)      
         self.Hold2 = 	self.Hold1                                       # Step holding potential (mV)
         self.ChangeClamp = self.tstop                                       # What time to change from hold1 to hold2 (ms)
-        
-        
-        # Individual Synapse Voltage (optional)
-        # self.DoIndSyn = 1                                               # Set to 1 to examine an individual synapse
-        # self.SynNum = 0                                                 #Index number of the 
-        
-        # Set the run mode (single run, multi run, ect.)
+
         self.RunMode = 1                                                #Run mode, 1 = single run, 2 = multiRun
         h.celsius = 32
         
         
     def initialize_passive_properties(self):
         """Initialize settings for passive properties"""
-        # self.celcius = 32
         self.v_init = -60#-37 #(after excitation)-46.7                    # What voltage to start the cell at (mV)
         # self.CM = 1.18                                                   # Membrance capacitance (uF/Cm2)
         # self.RA = 132                                                    # Axial resistance (Ohm cm2)
         # self.EPAS = -60                                                  # Equilibrium potential of passive conductances (mV)
         # self.G_PAS = 0.00003906                                          # Passive membrane conductance (mho cm2)
-        self.BaselineExc = 4  / 1000                                         # How much current to inject (pA), push to restin membrane potential of -46.7 mV (3.5 &)
+        self.BaselineExc = 0                                         # How much current to inject (pA), push to restin membrane potential of -46.7 mV (3.5 &)
         
     def initialize_excitation_inhibition(self):
         """Initialize settings for excitation and inhibition"""
         #Excitatory input
-        self.ExcAmp = 3 / 1000                                                # How much current to inject (pA)
+        self.ExcAmp = 0                                                # How much current to inject (pA)
         
         self.inhDecay = 2 #19.2                                           # decays time on inhibitory synapse (ms)
         self.InhSpikeFreq = 100                                              # Frequency of inhibitory spiking (hZ)
         self.InhNoise = 0                                               # 0 deterministic, 1 intervals have negexp distribution.
-        self.inhSynWeight = 1#0.0001                                  # Initial weight set for each inhibitory synapse
+        self.inhSynWeight = 0                                  # Initial weight set for each inhibitory synapse
         # self.SynWeight_MultiRun = 0.0001                                   # Weight each synapse is changed to 1 by 1 (Only applicable for LoopedRun)
         self.inhRevPot = -60                                                # Reversal Potential of each inhibitory synapse (mV)
         
