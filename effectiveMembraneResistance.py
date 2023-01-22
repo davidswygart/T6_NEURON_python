6e1a68d1566226ae6d532f25f6161a45cedcb51b
David Swygart <davidswygart2015@u.northwestern.edu>
2023-01-22 12:09:32 -0600

    clean up calculation of conductance
    better variable names so that I'm not confused

--- effectiveMembraneResistance.py
+++ effectiveMembraneResistance.py
@@ -49,62 +49,62 @@ segArea = np.expand_dims(np.array(segArea),1)
         
 plt.hist(segArea)
 
-#%% calculate conductance in each section
+#%% calculate conductance in each section and cumulative
 plt.figure()
 t = np.array(ex.time)
 
+gCaSiemenPerCm2 = np.array(ex.rec.gCa)
+gKvSiemenPerCm2 = np.array(ex.rec.gKv1_2)
+gHCN2SiemenPerCm2 = np.array(ex.rec.gHCN2)
+gLeakSiemenPerCm2 = np.full(np.shape(gCaSiemenPerCm2), T6.settings.g_pas) 
 
-# HCN2 and leak conductance are in units of S/cm2
+                            
+
+
+#%%
+# convert conductance from S/cm2 to S
 Um2Tocm2 = 1e-8
+gCaSiemens = gCaSiemenPerCm2 * segArea * Um2Tocm2
+gKvSiemens = gKvSiemenPerCm2 * segArea * Um2Tocm2
+gHCN2Siemens = gHCN2SiemenPerCm2 * segArea * Um2Tocm2
+gLeakSiemen = gLeakSiemenPerCm2 * segArea * Um2Tocm2
 
-gCa = np.array(ex.rec.gCa) * segArea * Um2Tocm2
-gCaTotal = np.sum(gCa, axis=0)
+# add up cumulative conductance over whole membrane
+gCaTotal = np.sum(gCaSiemens, axis=0)
+gKvTotal = np.sum(gKvSiemens, axis=0)
+gHCN2Total = np.sum(gHCN2Siemens, axis=0)
+gLeakTotal = np.sum(gLeakSiemen, axis=0)
+cellConductance = gCaTotal + gKvTotal + gHCN2Total + gLeakTotal
+
+# plot effective conductance
 plt.plot(t,gCaTotal)
-
-
-gKv = np.array(ex.rec.gKv1_2) * segArea * Um2Tocm2
-gKvTotal = np.sum(gKv, axis=0)
 plt.plot(t,gKvTotal)
-
-
-gHCN2 = np.array(ex.rec.gHCN2)  * segArea * Um2Tocm2
-gHCN2Total = np.sum(gHCN2, axis=0)
 plt.plot(t,gHCN2Total)
-        
-gLeak = T6.settings.g_pas * segArea * Um2Tocm2
-gLeakTotal = np.sum(gLeak)
-gLeakTotal = np.repeat(gLeakTotal, len(gHCN2Total), axis=0)
 plt.plot(t,gLeakTotal)
+plt.plot(t,cellConductance)
 
 plt.title('total conductances')
 plt.ylabel("S")
 plt.xlabel('ms')
-plt.legend(['Ca','Kv', 'Hcn2', 'leak'])
+plt.legend(['Ca','Kv', 'Hcn2', 'leak', 'total'])
 
-cellConductance = gCaTotal + gKvTotal + gHCN2Total + gLeakTotal
-
-# plot resistance
-
+# plot resistance (in GOhm)
+GOhm =  1e9
 plt.figure()
-plt.plot(t, 1/gLeakTotal / 1e6)
+plt.plot(t,1/gLeakTotal / GOhm)
+plt.plot(t,1/cellConductance / GOhm)
+plt.ylim(bottom=0)
 
-effectiveResistance = 1/cellConductance / 1e6;
-plt.plot(t, effectiveResistance)
 plt.title('effective membrane resistance')
-plt.ylabel("MOhm")
+plt.ylabel("GOhm")
 plt.xlabel('ms')
 plt.legend(['passive', 'active'])
 
 #%%%%%%%%%%%%%%%%%% increase leak conductance equally accross whole cell in place of active conductances %%%%%%%%%%%%%%%%%%%%%%%%
-
-
 # no active conductances
 T6.settings.hcn2_gpeak = 0
 T6.settings.Kv1_2_gpeak = 0
 T6.settings.Cav_L_gpeak = 0
-
-
-
 
 # set excitation so that average ribbon is -35 mV, and inh that drops to -45 mV
 T6.settings.excSyn['gmax'] = 2600 / 8 # conductance at each excitatory input synapse (8 total)
