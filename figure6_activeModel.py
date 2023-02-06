2a83eb6aca6b5f9cc1e23ec6361312c4cc03d535
David Swygart <davidswygart@gmail.com>
2023-02-06 16:27:14 -0600

    optimize inh for CSR Q4 = 1.1

--- figure6_activeModel.py
+++ figure6_activeModel.py
@@ -5,13 +5,22 @@ Created on Mon Feb  6 13:23:09 2023
 @author: david
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 23 14:33:51 2023
+#%% For calculating CSR
+def calcCSR(stimTimeV, preTimeV, inhV):
+    excDelta = stimTimeV - preTimeV
+    inhDelta = stimTimeV - inhV
+    CSR = excDelta / inhDelta
 
-@author: dis006
-"""
-
+    # sort CSR and split into quartiles
+    sortedCSR = np.sort(CSR, axis = 1)# sort by low to high suppression
+    
+    quartileN = round(len(CSR[0,:])/4)
+    
+    Q4Avg = np.mean(sortedCSR[:, 0 : quartileN], axis = 1)
+    Q1Avg = np.mean(sortedCSR[:, quartileN*3-1 : ], axis = 1)
+    diffQ4toQ1 = Q4Avg-Q1Avg
+    
+    return CSR, Q1Avg, Q4Avg, diffQ4toQ1
 
 #%%############# Create Model and Experiment #########################
 from T6_Sim import Type6_Model
@@ -31,28 +40,28 @@ T6.settings.hcn2_gpeak = .78
 ex = Experiment(T6)
 ex.tstop = 1500
 
-#%%################ Set Exc and Inhibition ################################## 
-#%% small spot (-45 mV -> -30 mV)
+#%%################ Set Exc ################################## (-45 mV -> -30 mV)
 T6.settings.excDark.frequency = 70
 T6.settings.excSyn.frequency = 500
 
 
 T6.settings.excSyn.gMax = 1.12e-5
 T6.settings.excDark.gMax = T6.settings.excSyn.gMax
-
 T6.settings.inhSyn.gMax = 0
-
 T6.update()
 ex.run()
+
 smallSpotVRib1 = np.array(ex.rec.ribV[0])
 smallSpotVRib2 = np.array(ex.rec.ribV[24])
 ex.makePlot(ex.time,smallSpotVRib1 ,  xmin = 100)
 preTimeV = ex.averageRibVoltage(startTimeMs=300, endTimeMs =500) #preTime average
 stimTimeV = ex.averageRibVoltage(startTimeMs=500, endTimeMs=1000) #postTime average
+excDelta = stimTimeV - preTimeV
 
 plt.hist(stimTimeV - preTimeV)
 plt.title('depolarization')
-#%% large spot (all inhibitory activated) (-39.1 mV == CSR 1.1)
+
+#%%########### l (all inhibitory activated) (-39.1 mV == CSR 1.1)
 T6.settings.inhSyn.frequency = 79
 
 T6.settings.inhSyn.gMax = 1e-5
@@ -94,7 +103,6 @@ bigSpotVRib1 = np.array(ex.rec.ribV[0])
 bigSpotVRib2 = np.array(ex.rec.ribV[24])
 
 #%% plot large and small simultaneously for both examples
-
 f, (ax1, ax2)  = plt.subplots(nrows = 2)
 
 ax1.plot(ex.time, smallSpotVRib1, label='no inh.')
@@ -113,36 +121,38 @@ plt.plot([500,500],[-60,-20])
 plt.plot([1000,1000],[-60,-20])
 plt.show()
 
-
 #%% ########### Figure 1C, turn on each inhbitory synapse individually ############## 
 ex.tstop = 1001 # No need to run experiment past stimulus time since I'm not using those datapoints
+T6.settings.inhSyn.gMax = 2.9e-5
 
 n=1
 inds = T6.nNearestInh(n)
-inhV = ex.loopThroughInhibitorySynapses(inds)
+inhV = ex.loopThroughInhibitorySynapses([inds[86]])    
+    
+CSR, Q1Avg, Q4Avg, diffQ4toQ1 = calcCSR(stimTimeV, preTimeV, inhV)
+print(np.mean(Q4Avg))
 
-#%% calculate suppression
-excDelta = stimTimeV - preTimeV
-inhDelta = inhV - preTimeV
-suppression = (1-inhDelta/excDelta)*100
 
-plt.hist(suppression[0,:]) #example histogram
 
-#%% sort suppression and split into quartiles
-sortedSuppression = np.sort(suppression, axis = 1)# sort by low to high suppression
 
-quartileN = round(len(sortedSuppression[1,:])/4)
+#np.argmin(abs(Q4Avg-np.median(Q4Avg))) == 86
 
-Q1Avg = np.mean(sortedSuppression[:, 0 : quartileN], axis = 1)
-Q4Avg = np.mean(sortedSuppression[:, quartileN*3-1 : ], axis = 1)
 
-suppressionDifference = Q4Avg-Q1Avg
-plt.hist(suppressionDifference)
-plt.xlabel('Q4-Q1 suppression (%)')
 
-#%% calculate CSR
-excDelta = stimTimeV - preTimeV
-inhDelta = stimTimeV - inhV
-CSR = excDelta / inhDelta
 
-plt.hist(CSR[119,:]) #example histogram
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
