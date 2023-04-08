562c9b04be9a0fce620400a99fbe0ce7e3019ab3
David Swygart <davidswygart@gmail.com>
2023-04-08 18:49:27 -0500

    Update range_depolarize.py

--- range_depolarize.py
+++ range_depolarize.py
@@ -1,68 +1,107 @@
-# -*- coding: utf-8 -*-
-# Test model over a range of depolarization steps
+def calcCSR(stimTimeV, preTimeV, inhV):
+    excDelta = stimTimeV - preTimeV
+    inhDelta = stimTimeV - inhV
+    CSR = excDelta / inhDelta
 
+    # sort CSR and split into quartiles
+    sortedCSR = np.sort(CSR, axis = 1)# sort by low to high suppression
+    
+    quartileN = round(len(CSR[0,:])/4)
+    
+    Q1Avg = np.mean(sortedCSR[:, 0 : quartileN], axis = 1)
+    Q4Avg = np.mean(sortedCSR[:, quartileN*3-1 : ], axis = 1)
+    diffQ4toQ1 = Q4Avg-Q1Avg
+    
+    return CSR, Q1Avg, Q4Avg, diffQ4toQ1
+
+#%%
+def runNewExc(inhG=1.62e-5, stimFreq=500, darkFreq=70, inds=[]):
+    T6.settings.excSyn.frequency = stimFreq
+    T6.settings.excDark.frequency = darkFreq
+    T6.settings.inhSyn.gMax = 0
+    T6.update()
+    ex.run()
+    preTimeV = ex.averageRibVoltage(startTimeMs=500, endTimeMs =999) #preTime average
+    excStimTimeV = ex.averageRibVoltage(startTimeMs=1000, endTimeMs=2000) #postTime average
+    
+    # run with inhibition only
+    # T6.settings.inhSyn.gMax = inhG
+    # inhV = ex.loopThroughInhibitorySynapses(inds)   
+    # CSR, Q1Avg, Q4Avg, diffQ4toQ1 = calcCSR(excStimTimeV, preTimeV, inhV)
+    # print('Q1 = ', np.median(Q1Avg))
+    # return diffQ4toQ1
+    return 1
+    
+    
+#%%############# Create Model and Experiment #########################
 from T6_Sim import Type6_Model
 from Experiment import Experiment
 import numpy as np
+import matplotlib.pyplot as plt
 
-#%%%%%%%%%%%%%%%%%% Active Model %%%%%%%%%%%%%%%%%%%%%%%%
+#%% Only make the model once. NEURON can do weird things if you remake it
 T6 = Type6_Model()
-T6.settings.Cav_L_gpeak = 1.62
-T6.settings.hcn2_gpeak = .78
-T6.settings.Kv1_2_gpeak = 12
-
 ex = Experiment(T6)
+ex.tstop = 2001
 
-T6.settings.excSyn['start'] = 200
-T6.settings.inhSyn['start'] = 400
-T6.settings.excSyn['darkProp'] = 0.2 # proportion that is dark current
-ex.tstop = 600
+#%% start with simulating all inhibitory synapses
+n=120
+inds = T6.nNearestInh(n)
 
-#%%%%%%%%%%%%%%%%%% depol = -43 %%%%%%%%%%%%%%%%%%%%%%%%
-T6.settings.excSyn['gmax'] = 922 / 8 # conductance at each excitatory input synapse (8 total)
-T6.settings.inhSyn['gmax'] = 1100  #conductance at single inhibitory synapse
-T6.update()
-data = ex.LoopThoughInhibitorySynapses(folder = 'results\\range\\depol\\n43_');
-#data = ex.LoopThoughInhibitorySynapses(inhInds=[16]);
+diffs120 = [];
+#%% 
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=50, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=100, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=200, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=300, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=400, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=500, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=600, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=800, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=1000, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=1400, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=1800, darkFreq=70, inds=inds[[0]]))
+diffs120.append(runNewExc(inhG=1.62e-5, stimFreq=3600, darkFreq=70, inds=inds[[0]]))
 
-#%%%%%%%%%%%%%%%%%% depol = -40 %%%%%%%%%%%%%%%%%%%%%%%%
-T6.settings.excSyn['gmax'] = 1350 / 8 # conductance at each excitatory input synapse (8 total)
-T6.settings.inhSyn['gmax'] = 3100  #conductance at single inhibitory synapse
-T6.update()
-data = ex.LoopThoughInhibitorySynapses(folder = 'results\\range\\depol\\n40_');
-#data = ex.LoopThoughInhibitorySynapses(inhInds=[16]);
+#%%
+n=1
+inds = T6.nNearestInh(n)
 
-#%%%%%%%%%%%%%%%%%% depol = -35 %%%%%%%%%%%%%%%%%%%%%%%%
-T6.settings.excSyn['gmax'] = 2600 / 8 # conductance at each excitatory input synapse (8 total)
-T6.settings.inhSyn['gmax'] = 8000  #conductance at single inhibitory synapse
-T6.update()
-data = ex.LoopThoughInhibitorySynapses(folder = 'results\\range\\depol\\n35_');
-#data = ex.LoopThoughInhibitorySynapses(inhInds=[16]);
+diffs1 = [];
 
-#%%%%%%%%%%%%%%%%%% depol = -30 %%%%%%%%%%%%%%%%%%%%%%%%
-T6.settings.excSyn['gmax'] = 5300 / 8 # conductance at each excitatory input synapse (8 total)
-T6.settings.inhSyn['gmax'] = 14000  #conductance at single inhibitory synapse
-T6.update()
-data = ex.LoopThoughInhibitorySynapses(folder = 'results\\range\\depol\\n30_');
-#data = ex.LoopThoughInhibitorySynapses(inhInds=[16]);
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=50, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=100, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=200, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=300, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=400, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=500, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=600, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=800, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=1000, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=1400, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=1800, darkFreq=70, inds=inds))
+diffs1.append(runNewExc(inhG=1.62e-5, stimFreq=3600, darkFreq=70, inds=inds))
 
-#%%%%%%%%%%%%%%%%%% depol = -25 %%%%%%%%%%%%%%%%%%%%%%%%
-T6.settings.excSyn['gmax'] = 11600 / 8 # conductance at each excitatory input synapse (8 total)
-T6.settings.inhSyn['gmax'] = 22000  #conductance at single inhibitory synapse
-T6.update()
-data = ex.LoopThoughInhibitorySynapses(folder = 'results\\range\\depol\\n25_');
-#data = ex.LoopThoughInhibitorySynapses(inhInds=[16]);
 
-#%%%%%%%%%%%%%%%%%% depol = -20 %%%%%%%%%%%%%%%%%%%%%%%%
-T6.settings.excSyn['gmax'] = 30000 / 8 # conductance at each excitatory input synapse (8 total)
-T6.settings.inhSyn['gmax'] = 30000  #conductance at single inhibitory synapse
-T6.update()
-data = ex.LoopThoughInhibitorySynapses(folder = 'results\\range\\depol\\n20_');
-#data = ex.LoopThoughInhibitorySynapses(inhInds=[16]);
 
-#%%%%%%%%%%%%%%%%%% depol = -15 %%%%%%%%%%%%%%%%%%%%%%%%
-T6.settings.excSyn['gmax'] = 150000 / 8 # conductance at each excitatory input synapse (8 total)
-T6.settings.inhSyn['gmax'] = 38000  #conductance at single inhibitory synapse
-T6.update()
-data = ex.LoopThoughInhibitorySynapses(folder = 'results\\range\\depol\\n15_');
-#data = ex.LoopThoughInhibitorySynapses(inhInds=[16]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
